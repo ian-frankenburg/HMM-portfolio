e25b8f5f91e298955988baae8d9bb885885d8571
Ian Frankenburg <31015994+ian-frankenburg@users.noreply.github.com>
2021-12-29 16:48:02 -0500

    working on posterior predictive dist of asset returns

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# HMMbayes
-A bare-bones implementation of a multivariate Bayesian Hidden Markov Model 
+# HMMbayes Overview
+This repository contains a Bayesian implementation of a 2-state multivariate Hidden Markov Model (HMM). Code correctness is tested within the ```test``` folder, with a simulated example demonstrating correct parameter recovery with a fixed ground truth. For a real-world example, a simple mean-variance allocation scheme is implementated to demonstrate rebalancing an all-ETF portfolio during different market regimes. Further functionality to implement a $K$-state multivariate HMM will be added in the future.
+
+Source code is located within the ```src``` folder, with all methods entirely self-contained within a single ```.cpp``` file. At the expense of a small computational overhead, parallel computing is supported within the ```HMM_parallel``` file to allow for simultaneous MCMC chains to be run concurrently on separate threads.
+
+# Functionality
+These instructions will assume installation and use of RStudio. After the ```.cpp``` is compiled, a single R function labeled ```gibbs``` or ```gibbs_parallel``` will be available within the workspace. Simply provide the requisite arguments that include the data and prior fixed hyperparameters. 
+
+# Example
+Past the test cases within the ```testing``` directory, there is an application to portfolio allocation by way of calculating the posterior predictive density of the returns for a collection of assets.Given this distribution, various optimization methodology can be employed to dynamically allocate to the various assets. 
+
+
+
--- src/HMM.cpp
+++ src/HMM.cpp
@@ -1,15 +1,13 @@
-#define ARMA_NO_DEBUG
 #include <RcppDist.h>
 #include <RcppThread.h>
 // [[Rcpp::depends(RcppArmadillo, RcppDist, RcppThread)]]
 
 using namespace arma;
 
-mat forward(const mat& y, const mat& mu, const cube& Sigma, const mat& pr, const double init0)
+void forward(mat& filter, mat& predict, mat& likelihood, vec& marginal,
+             const mat& y, const mat& mu, const cube& Sigma, const mat& pr, const double init0)
 {
   int T = y.n_rows;
-  mat filter(T, 2, fill::zeros), predict(T, 2, fill::zeros), likelihood(T, 2, fill::zeros);
-  vec marginal(T, fill::zeros);
   double p00 = pr(0,0);
   double p11 = pr(1,1);
   double init1 = 1 - init0;
@@ -47,28 +45,18 @@ mat forward(const mat& y, const mat& mu, const cube& Sigma, const mat& pr, const
       filter(t,1) = likelihood(t,1) * predict(t,1) / marginal(t);
     }
   }
-  return(filter);
 }
 
-vec backward1(const mat& filter, const mat& pr)
+void backward(const mat& filter, const mat& pr, vec& s, vec& n)
 {
-  int T = filter.n_rows;
-  double pr0;
-  vec s(T, fill::zeros);
+  int T = filter.n_rows; double pr0;
+  n.zeros(4);
   pr0 = filter(T-1, 0);
-  s(T-1) = (R::runif(0, 1) < pr0) ? 0 : 1;
+  s(T-1) = (R::runif(0, 1) < pr0) ? 0 : 1;;
+  (s(T-1)==0) ? n(0)++: n(3)++;
   for(int t = (T-2); t >= 0; t--){
     pr0 = pr(0,s(t+1))*filter(t, 0) / dot(pr.col(s(t+1)), filter.row(t));
     s(t) = (R::runif(0,1)<pr0) ? 0 : 1;
-  }
-  return(s);
-}
-
-vec backward2(const vec& s)
-{
-  int T = s.n_elem;
-  vec n(4, fill::zeros);
-  for(int t = (T-2); t >= 0; t--){
     if(s(t+1)==0 && s(t)==0){
       n(0)++;
     }else if(s(t+1)==1 && s(t)==0){
@@ -79,23 +67,21 @@ vec backward2(const vec& s)
       n(3)++;
     }
   }
-  return(n);
 }
 
-mat transition(const double& n00, const double& n10, const double& n01, const double& n11)
+void transition(const int& n00, 
+                const int& n10, const int& n01, const int& n11, mat& pr)
 {
-  mat pr(2, 2, fill::eye);
   int u00 = 5, u01 = 1, u10 = 1, u11 = 5;
   double p = R::rbeta(u11 + n11, u10 + n10);
   double q = R::rbeta(u00 + n00, u01 + n01);
   pr(0,0) = q;
-  pr(0,1) = 1 - q;
-  pr(1,0) = 1 - p;
+  pr(0,1) = 1-q;
+  pr(1,0) = 1-p;
   pr(1,1) = p;
-  return(pr);
 }
 
-mat colMeans(const mat& y) 
+mat colMeans(mat y) 
 {
   mat x(y.n_cols, 1, fill::zeros);
   for(uword i=0; i<y.n_cols; i++){
@@ -104,21 +90,22 @@ mat colMeans(const mat& y)
   return(x);
 }
 
-mat colDiff(const mat& y, const vec& mu) 
+mat colDiff(mat y, vec mu) 
 {
   mat x(y.n_rows, y.n_cols, fill::zeros);
-  for(uword i=0; i < y.n_cols; i++){
-    x.col(i) = y.col(i) - mu(i);
+  for(uword i=0; i<y.n_cols; i++){
+    x.col(i) = y.col(i)-mu(i);
   }
   return(x);
 }
 
-mat location(const mat& y, const vec& s, const mat& mu0,
+void location(const mat& y, const vec& s, const mat& mu0, mat&mu_draw,
               const cube& Sigma0, const cube& Sigma1)
 {
+  
   uword p = y.n_cols;
   vec nn(2, fill::zeros);
-  mat mu1(p,2, fill::zeros), mu_draw(p, 2, fill::zeros);
+  mat mu1(p,2, fill::zeros);
   cube Lambda(p, p, 2, fill::zeros);
   
   nn(0) =  uvec(find(s==0)).n_elem;
@@ -133,14 +120,11 @@ mat location(const mat& y, const vec& s, const mat& mu0,
   
   mu_draw.col(0) = rmvnorm(1, mu1.col(0), Lambda.slice(0)).t();
   mu_draw.col(1) = rmvnorm(1, mu1.col(1), Lambda.slice(1)).t();
-  
-  return(mu_draw);
 }
 
-cube scale(const mat& y, const vec& s, const vec& v0, const mat& mu, const cube& S0)
+void scale(const mat& y, const vec& s, const vec& v0, const mat& mu, const cube& S0, cube& S)
 {
   uword p = y.n_cols;
-  cube S(p,p,2,fill::zeros);
   mat S1_0(p,p,fill::zeros);
   uvec s0 = find(s==0);
   S1_0 = S0.slice(0) + colDiff(y.rows(s0), mu.col(0)).t()*colDiff(y.rows(s0), mu.col(0));
@@ -153,75 +137,52 @@ cube scale(const mat& y, const vec& s, const vec& v0, const mat& mu, const cube&
   mat W1 = inv(rwish(v0(1)+s1.n_elem, inv(S1_1)));
   
   S = join_slices(W0,W1);
-  return(S);
 }
 
 // [[Rcpp::export]]
-Rcpp::List gibbs(const uword& niter, const uword& burnin, const uword& num_chains,
-                 const mat& y, const cube& Sigma0, const vec& v0,
+Rcpp::List gibbs(const uword& niter, const uword& burnin, const mat& y, 
+                 const cube& Sigma0, const vec& v0,
                  const mat& mu0, const cube& S0)
 {
   uword T = y.n_rows;
   int p = y.n_cols;
-  vec init0 = Rcpp::runif(num_chains, 0, 1);
-  cube pr_st(2, 2, num_chains, fill::zeros);
-  for(int c=0; c<num_chains; c++){
-    pr_st.slice(c)(0,0) = R::rbeta(0,1);
-    pr_st.slice(c)(0,1) = 1-pr_st.slice(c)(0,0);
-    pr_st.slice(c)(1,1) = R::rbeta(0,1);
-    pr_st.slice(c)(1,0) = 1-pr_st.slice(c)(1,1);
-  }
+  double init0 = R::runif(0, 1);
   
-  mat n(4, num_chains, fill::zeros), s(T, num_chains, fill::zeros);
-  vec marginal(T, fill::zeros);
+  mat filter(T, 2, fill::zeros), predict(T, 2, fill::zeros), likelihood(T, 2, fill::zeros), mu(p,2,fill::zeros);
+  vec marginal(T, fill::zeros), s(T, fill::zeros), n(4, fill::zeros);
   
-  cube mu0_save(p, niter-burnin, num_chains, fill::zeros), mu1_save(p, niter-burnin, num_chains, fill::zeros);
+  mat mu0_save(p, niter-burnin, fill::zeros), mu1_save(p, niter-burnin, fill::zeros), pr(2, 2, fill::zeros);
+  pr(0, 0) = R::rbeta(0, 1);
+  pr(0, 1) = 1 - pr(0, 0);
+  pr(1, 1) = R::rbeta(0, 1);
+  pr(1, 0) = 1 - pr(1, 1);
 
-  cube pr(2, 2, num_chains, fill::zeros);
-  for(int c=0; c<num_chains; c++){
-    pr.slice(c) = pr_st.slice(c);
-  }
-  cube filter(T, 2, num_chains, fill::zeros), mu(p, 2, num_chains, fill::zeros);
+  cube Sigma0_save(p, p, niter-burnin, fill::zeros), Sigma1_save(p, p, niter-burnin, fill::zeros), 
+  filter_save(T, 2, niter-burnin, fill::zeros),
+  pr_save(2, 2, niter-burnin, fill::zeros), S(p, p,2,fill::zeros);
+  S.slice(0) = S.slice(1) = eye(p, p);
   
-  field<cube> Sigma1(num_chains), filter_save(num_chains), 
-  Sigma0_save(num_chains), Sigma1_save(num_chains), pr_save(num_chains);
-  
-  cube blank(p, p, 2, fill::zeros), blank2(p, p, niter-burnin, fill::zeros),
-  blank3(T, 2, niter-burnin, fill::zeros), blank4(2, 2, niter-burnin, fill::zeros);
-  
-  for(int i=0; i<2; i++){
-    blank.slice(i) = eye(p, p);
-  }
-  Sigma1.fill(blank);
-  filter_save.fill(blank3);
-  Sigma0_save.fill(blank2);
-  Sigma1_save.fill(blank2);
-  pr_save.fill(blank4);
-  
-  RcppThread::parallelFor(0, num_chains, [&] (int chain){
-    for(uword i=0; i < niter; i++){
-      // // step one: forward (discrete state Kalman) filter
-      filter.slice(chain) = forward(y, mu.slice(chain), Sigma1(chain), pr.slice(chain), init0(chain));
-      // // step two: backward sample
-      s.col(chain) = backward1(filter.slice(chain), pr.slice(chain));
-      n.col(chain) = backward2(s.col(chain));
-      // // step three: sample transition parameters
-      pr.slice(chain) = transition(n(0, chain), n(1, chain), n(2, chain), n(3, chain));
-      // // step four: sample mean and (co)variance parameters
-      mu.slice(chain) = location(y, s.col(chain), mu0, Sigma0, Sigma1(chain));
-      // // sample scale parameters
-      Sigma1(chain) = scale(y, s.col(chain), v0, mu.slice(chain), S0);
-      // save MCMC iterations for each chain
-      if(i >= burnin){
-        filter_save(chain).slice(i-burnin) = filter.slice(chain);
-        pr_save(chain).slice(i-burnin) = pr.slice(chain);
-        mu0_save.slice(chain).col(i-burnin) = mu.slice(chain).col(0);
-        mu1_save.slice(chain).col(i-burnin) = mu.slice(chain).col(1);
-        Sigma0_save(chain).slice(i-burnin) = Sigma1(chain).slice(0);
-        Sigma1_save(chain).slice(i-burnin) = Sigma1(chain).slice(1);
-      }
+  for(uword i=0; i < niter; i++){
+    // // step one: forward filter
+    forward(filter, predict, likelihood, marginal, y, mu, S, pr, init0);
+    // // step two: backward sample
+    backward(filter, pr, s, n);
+    // // step three: sample transition parameters
+    transition(n(0), n(1), n(2), n(3), pr);
+    // // step four: sample mean parameters
+    location(y, s, mu0, mu, Sigma0, S);
+    // // step five: sample scale parameters
+    scale(y, s, v0, mu, S0, S);
+    // // save MCMC draws after burnin period
+    if(i >= burnin){
+      filter_save.slice(i-burnin) = filter;
+      pr_save.slice(i-burnin) = pr;
+      mu0_save.col(i-burnin) = mu.col(0);
+      mu1_save.col(i-burnin) = mu.col(1);
+      Sigma0_save.slice(i-burnin) = S.slice(0);
+      Sigma1_save.slice(i-burnin) = S.slice(1);
     }
-  });
+  }
   Rcpp::List out(6);
   out["filter"] = filter_save;
   out["mu0_save"] = mu0_save;
